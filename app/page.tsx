6a14eb039ea2a9178a39b5ca905f317186fa5323
suman das <sumankrdas1999@gmail.com>
2026-02-02 18:21:39 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import IntroSection from "@/component/Home/IntroSection";
 import DesktopLogoSlider from "@/component/Home/logoSliderdesktop";
+import BannerSlider from "@/component/Home/BannerSlider";
+import OurWork from "@/component/Home/OurWork";
 
 export default function Home() {
   return (
     <>
       <IntroSection />
       <DesktopLogoSlider />
+      <BannerSlider />
+      <OurWork />
     </>
   );
 }
